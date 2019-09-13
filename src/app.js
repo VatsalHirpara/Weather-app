1e6ecf6f34b5c172eb21691e7c3ebbc1576ca67b
Vatsal Hirpara <vatsalhirpara@yahoo.com>
2019-09-14 01:04:38 +0530

    now it will work

--- src/app.js
+++ src/app.js
@@ -23,14 +23,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Vatsal Hirpara'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Vatsal Hirpara'
     })
 })
 
@@ -38,7 +38,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Vatsal Hirpara'
     })
 })
 
@@ -65,19 +65,6 @@ app.get('/weather', (req, res) => {
                 address: req.query.address
             })
         })
-    })
-})
-
-app.get('/products', (req, res) => {
-    if (!req.query.search) {
-        return res.send({
-            error: 'You must provide a search term'
-        })
-    }
-
-    console.log(req.query.search)
-    res.send({
-        products: []
     })
 })
 
